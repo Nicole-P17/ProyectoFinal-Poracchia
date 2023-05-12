32818b5361b5ba86adaa80be671d55f13dff8593
Nicole-P17 <poracchian@gmail.com>
2023-05-11 22:29:43 -0300

    agrego hasta un poco de herencias

--- AppTerapia/urls.py
+++ AppTerapia/urls.py
@@ -5,11 +5,11 @@ from AppTerapia.views import saludo, pacientes, Minombre
 from AppTerapia.views import listar_consultantes, listar_pacientes, listar_psicologos, probandoTemplate
 
 urlpatterns = [
-    path('saludo/', saludo),
-    path('pacientes/', pacientes),
-    path('Minombre/<nombre>', Minombre),
-    path("VerHtml/", probandoTemplate),
-    path("lista-consultantes", listar_consultantes),
-    path("lista-pacientes", listar_pacientes),
-    path("lista-psicologos", listar_psicologos),
+    path('saludo/', views.saludo, name="saludo"),
+    path('pacientes/', views.pacientes ,name="pacientes"),
+    path('Minombre/<nombre>', views.Minombre, name="Minombre"),
+    path("VerHtml/", views.probandoTemplate, name="probandoTemplate"),
+    path("lista-consultantes", views.listar_consultantes, name="listar_consultantes"),
+    path("lista-pacientes", views.listar_pacientes, name="listar_pacientes"),
+    path("lista-psicologos", views.listar_psicologos, name="listar_psicologos"),
 ]
